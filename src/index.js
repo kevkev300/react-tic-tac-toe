802f48472e22579cea0922a963c70e2a7c65b340
Kevin Liebholz <liebholz@crowddesk.de>
2021-10-11 08:23:46 +0200

    WIP: create squares with loops

--- src/index.js
+++ src/index.js
@@ -18,24 +18,27 @@ class Board extends React.Component {
     />;
   }
 
+  createRows() {
+    let content = []
+    for (let i = 0; i < 3; i++) {
+      let counter = i * 3
+      content.push(
+        <div className="board-row">
+          {this.renderSquare(counter)}
+          {this.renderSquare(counter + 1)}
+          {this.renderSquare(counter + 2 )}
+        </div>
+      )
+    }
+    return content
+  }
+
   render() {
     return (
       <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+        {
+          this.createRows()
+        }
       </div>
     );
   }
